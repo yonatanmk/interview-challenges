5dfc1faf8d8f497d7a1f0c9dbc04a10213000d02
yonatanmk <yonatanmk@gmail.com>
2022-11-04 23:49:38 -0700

    display playlist items

--- react-coding-challenges/spootify/src/routes/Discover/components/Discover.js
+++ react-coding-challenges/spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import "../styles/_discover.scss";
+import axios from "axios";
 
 export default class Discover extends Component {
   constructor() {
@@ -9,8 +10,48 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
     };
+  }
+
+  async componentDidMount() {
+    const token = process.env.REACT_APP_BEARER_TOKEN;
+    // const token2 = process.env.REACT_APP_BEARER_TOKEN;
+    // console.log("TOKEN");
+    // console.log({ token, token2 });
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        // "Content-Type": "application/json",
+      },
+    };
+
+    // const resp1 = await axios.get("https://api.spotify.com/v1/me", config);
+
+    // const userId = resp1.data.id;
+
+    // const resp = await axios.get(
+    //   "https://api.spotify.com/v1/browse/featured-playlists",
+    //   config
+    // );
+
+    // const resp = await axios.get(
+    //   `https://api.spotify.com/v1/users/${userId}/playlists`,
+    //   config
+    // );
+
+    const resp = await axios.get(
+      `https://api.spotify.com/v1/me/playlists`,
+      config
+    );
+
+    // this.setState({
+    //   playlists: resp.data.playlists.items,
+    // });
+
+    this.setState({
+      playlists: resp.data.items,
+    });
   }
 
   render() {
@@ -18,9 +59,22 @@ export default class Discover extends Component {
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        <DiscoverBlock
+          text="RELEASED THIS WEEK"
+          id="released"
+          data={newReleases}
+        />
+        <DiscoverBlock
+          text="FEATURED PLAYLISTS"
+          id="featured"
+          data={playlists}
+        />
+        <DiscoverBlock
+          text="BROWSE"
+          id="browse"
+          data={categories}
+          imagesKey="icons"
+        />
       </div>
     );
   }
