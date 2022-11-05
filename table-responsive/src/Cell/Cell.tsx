79b13792cd67b9eedac951927a146d27c232e770
yonatanmk <yonatanmk@gmail.com>
2022-11-05 14:40:47 -0700

    table-responsive sorganize files

---
+++ table-responsive/src/Cell/Cell.tsx
@@ -0,0 +1,11 @@
+import "./Cell.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ table-responsive/src/Row/Row.tsx
@@ -0,0 +1,11 @@
+import "./Row.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default App;
